1ba857b3deaf7f88311b4beecfed7ebdcbe43ce3
Patryk Górski <patryk.gorski@rtbhouse.com>
2015-09-13 16:36:09 +0200

    New section newsletter and edit animation text code

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# notify-theme
+# Notify Theme (RWD) - UWAGA PRACE CIĄGLE TRWAJĄ.
+---
+
+Motyw o tematyce aplikacji mobilnych a także projektów.\
+Projekt graficzny i pliki PSD wykonane przez Michael Reimer | www.bestpsdfreebies.com \
+Cięcie i kodowanie szablonu wykonane przez Patryk Górski.
+
+Cechy szablonu:
+ - Responsywny design
+ - Animacja tekstu pod logotypem jak i w sekcji "opinie".
+ - Newsletter
+ - Odnośniki do portalów społecznościowych
+ - Możliwość prostej edycji animacji tekstów.
+ - Możliwość podpięcia strony pod różne systemy CMS.
+ - Sekcja opinii.
+ - Możliwość dodania slajdów imitujących działanie telefonu.
+ - PLIKI SASS
+
+
+ #### Tutorial 1. Edycja slajdów:
+ Otwórz plik html i znajdź element "slide-text" lub "op-text".\
+ Dodaj wewnątrz tego elementu nowy paragraf i wstaw w nim swój tekst.\
+index.html:
+
+```html
+	<p>Mój nowy paragraf z nowym tekstem</p>
+```
+
+#### Tutorial 2. Edycja animacji tekstu:
+Java Script:
+```javascript
+        var headerSlide = new slideText();
+        headerSlide.slideStartFun(".slide-text p"); // Element
+        headerSlide.slideAnimationTime = 200; // Time animation default 500 ms
+        headerSlide.slideTime = 3000; // How long slide show default 4000 ms 
+        headerSlide.slideStart = 0; // What first slide
+```
+
+
+### Biblioteki/Freemworki:
+
+ * [jQuery](https://jquery.com)
+ * [Notify PSD Theme](http://graphicburger.com/notify-psd-theme/) 
+ * [Foundation 5](http://foundation.zurb.com/)
+ * [Font Awesome](https://fortawesome.github.io/Font-Awesome/) 
+ * [SASS](http://sass-lang.com)
--- js/notifyAnimate.js
+++ js/notifyAnimate.js
@@ -41,28 +41,82 @@ var preloaders = {
 /* END PRELOADERS */
 
 /* 2. Slide*/
+function slideText(){
+	this.slideStart = this.slideStart || 0,
+	this.slideAnimationTime = this.slideAnimationTime || 500,
+	this.slideTime = this.slideTime || 4000,
+	this.slideTypeAnimations = '',
+	this.slideLength = 0,
+	this.slideStartFun = function(s){
+		var obj = this;
+		$(s).eq(this.slideStart).fadeIn(300, function(){
+			setTimeout(function(){
+				obj.slide(s, obj);
+			}, obj.slideTime);
+		});
 
+	},
+	this.slide = function(s, o){
+		this.slideLength = $(s).length;
+		$(s).eq(o.slideStart).fadeOut(o.slideAnimationTime, function(){
+			if(o.slideLength-1<=o.slideStart){
+				o.slideStart=0;	
+			}else{
+				o.slideStart++;
+			}
+			$(s).eq(o.slideStart).fadeIn(o.slideAnimationTime, function(){
+				setTimeout(function(){
+					o.slide(s, o);
+				}, o.slideTime);
+			});
+		});
+	}
+}
+
+/*
+$('.slide-text p').eq(headerSlide.slideStart).fadeIn(300, function(){
+	setTimeout(function(){
+		headerSlide.slide(".slide-text p", headerSlide);
+	}, headerSlide.slideTime);
+});
+
+$('.op-text p').eq(opinionSlide.slideStart).fadeIn(300, function(){
+	setTimeout(function(){
+ 		opinionSlide.slide(".op-text p", opinionSlide);
+ 	}, opinionSlide.slideTime);
+ });
+
+/* END SLIDE */
+
+/*
 var slideText = {
 	slideStart:this.slideStart || 0,
 	slideAnimationTime:this.slideAnimationTime || 500,
 	slideTime:this.slideTime || 4000,
 	slideTypeAnimations:'',
-	slideLength:$('.slide-text p').length,
-	slide:function(){
-		$('.slide-text p').eq(slideText.slideStart).fadeOut(slideText.slideAnimationTime, function(){
+	slideLength:0,
+	slide:function(s){
+		this.slideLength = $(s).length;
+		$(s).eq(slideText.slideStart).fadeOut(slideText.slideAnimationTime, function(){
 			if(slideText.slideLength-1<=slideText.slideStart){
 				slideText.slideStart=0;	
 			}else{
 				slideText.slideStart++;
 			}
 
-			$('.slide-text p').eq(slideText.slideStart).fadeIn(slideText.slideAnimationTime, function(){
-				setTimeout(slideText.slide, slideText.slideTime);
+			$(s).eq(slideText.slideStart).fadeIn(slideText.slideAnimationTime, function(){
+				setTimeout(function(){
+					slideText.slide(".slide-text p");
+				}, slideText.slideTime);
 			});
 		});
 	}
 }
 
 $('.slide-text p').eq(slideText.slideStart).fadeIn(300, function(){
-	setTimeout(slideText.slide, 2000);
+	setTimeout(function(){
+		slideText.slide(".slide-text p");
+	}, 2000);
 });
+*/
+
